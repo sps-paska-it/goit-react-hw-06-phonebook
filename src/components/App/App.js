9efe8c5eb74f3cbab26926584ddc1bf61d08a312
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-04-24 18:59:14 +0300

    toastify 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,16 +23,16 @@ export class App extends React.Component {
             }));
             return;
         }
-        toast.warn(`${newContact.name} is already in contacts`, {
-            position: 'top-right',
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: 'light',
-        });
+        // toast.warn(`${newContact.name} is already in contacts`, {
+        //     position: 'top-right',
+        //     autoClose: 5000,
+        //     hideProgressBar: false,
+        //     closeOnClick: true,
+        //     pauseOnHover: true,
+        //     draggable: true,
+        //     progress: undefined,
+        //     theme: 'light',
+        // });
     };
 
     deleteContact = id => {
